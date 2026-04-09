0903d2ad6ab17abdf2b21a8a97ea8f3b4e998ab5
Mohamed Suleyman Ibrahim <mohamedsuleyman813@gmail.com>
2026-04-09 16:26:08 +0300

    added end point of /posts method of post in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,15 @@ let express = require("express");
 
 let app = express();
 
-app.get("/posts" ,(req ,res)=>{
-    res.json("wuu shaqeeyay");
-})
+app.get("/posts", (req, res) => {
+  res.json("wuu shaqeeyay");
+});
 
+app.post("/posts", (req, res) => {
+  res.json({
+    status: true,
+    message: "posted",
+  });
+});
 
-module.exports =app;
+module.exports = app;
