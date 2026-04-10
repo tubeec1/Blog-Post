c99b9ad33d9102b489f077bc063fa02fe367a269
Mohamed Suleyman Ibrahim <mohamedsuleyman813@gmail.com>
2026-04-10 12:49:08 +0300

    added .env file and set database connection in conn.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
+let dotenv = require("dotenv");
+dotenv.config();
 let app = require("./app");
 
 let port = 5000;
 
-app.listen(port,()=>{
-    console.log(`server waxaa laga dhageysanayaa ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server waxaa laga dhageysanayaa ${port}`);
+});
