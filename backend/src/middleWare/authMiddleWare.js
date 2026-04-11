4617dac8dfe206c7483ba1ebfc643a2ba66ae0e5
Mohamed Suleyman Ibrahim <mohamedsuleyman813@gmail.com>
2026-04-12 02:34:56 +0300

    correcting authMiddlware name

--- backend/src/middleWare/authMiddleWare.js
+++ backend/src/middleWare/authMiddleWare.js
@@ -1,11 +1,11 @@
-let jwtHandler = require("../utilits/jwt")
-let AppError = require("../utilits/AppError")
+let jwtHandler = require("../utilits/jwt");
+let AppError = require("../utilits/AppError");
 
-let authMidle = async (req, res, next) => {
+let authMiddleware = async (req, res, next) => {
   let authorization = req.headers.authorization;
 
   if (!authorization) {
-    throw new AppError("There is no token", 401)
+    throw new AppError("There is no token", 401);
   }
   let token = authorization.split(" ")[1];
   let response = await jwtHandler.verifyToken(token);
@@ -13,4 +13,4 @@ let authMidle = async (req, res, next) => {
   next();
 };
 
-module.exports = authMidle;
+module.exports = authMiddleware;
