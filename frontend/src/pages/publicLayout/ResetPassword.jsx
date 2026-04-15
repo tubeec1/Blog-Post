2535a5113f9170467899fc106de4b1824105bf7c
Mohamed Suleyman Ibrahim <mohamedsuleyman813@gmail.com>
2026-04-15 13:24:05 +0300

    udating reset password

--- frontend/src/pages/publicLayout/ResetPassword.jsx
+++ frontend/src/pages/publicLayout/ResetPassword.jsx
@@ -20,7 +20,7 @@ let inputs = [
 ];
 const ResetPassword = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+    <div className="md:min-h-screen flex items-center justify-center bg-gray-100 p-4">
       <div className="w-full max-w-md bg-white border border-gray-200 rounded-2xl shadow-lg p-8">
         <h2 className="text-2xl font-bold text-center mb-6 text-gray-800">
           Reset Password
