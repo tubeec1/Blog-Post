d37f11b9d64c33dc481f21dd40f3a2b6aefaf7a7
Mohamed Suleyman Ibrahim <mohamedsuleyman813@gmail.com>
2026-04-15 12:41:37 +0300

    updating login and signup

--- frontend/src/pages/publicLayout/Login.jsx
+++ frontend/src/pages/publicLayout/Login.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 
 const Login = () => {
-  return(
- 
-     <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
       <div className="w-full max-w-md bg-white border border-gray-200 rounded-2xl shadow-lg p-8 text-gray-800">
         <h2 className="text-3xl font-bold text-center mb-6">Login </h2>
 
@@ -26,15 +25,6 @@ const Login = () => {
             />
           </div>
 
-          <div>
-            <label className="text-sm">Confirm Password</label>
-            <input
-              type="password"
-              placeholder="Confirm your password"
-              className="w-full mt-1 p-3 rounded-xl bg-gray-50 border border-gray-300 outline-none"
-            />
-          </div>
-
           <button
             type="button"
             className="w-full bg-gray-800 text-white font-semibold py-3 rounded-xl hover:bg-gray-700 transition"
@@ -44,13 +34,14 @@ const Login = () => {
         </form>
 
         <p className="text-center mt-6 text-sm text-gray-600">
-          Don't have an account? <span className="underline font-semibold cursor-pointer">Sign Up</span>
+          Don't have an account?{" "}
+          <span className="underline font-semibold cursor-pointer">
+            Sign Up
+          </span>
         </p>
       </div>
     </div>
-
-
-  )
+  );
 };
 
 export default Login;
--- frontend/src/pages/publicLayout/Signup.jsx
+++ frontend/src/pages/publicLayout/Signup.jsx
@@ -1,38 +1,61 @@
 import React from "react";
 
+let inputs = [
+  {
+    label: "Name",
+    placeholder: "Enter your name",
+    type: "text",
+  },
+  {
+    label: "Email",
+    placeholder: "Enter your email",
+    type: "email",
+  },
+  {
+    label: "Gender",
+    options: ["Male", "Female"],
+  },
+  {
+    label: "Password",
+    placeholder: "Enter your password",
+    type: "password",
+  },
+  {
+    label: "Confirm Password",
+    placeholder: "Confirm your password",
+    type: "password",
+  },
+];
 const Signup = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
       <div className="w-full max-w-md bg-white border border-gray-200 rounded-2xl shadow-lg p-8">
-        <h2 className="text-2xl font-bold text-center mb-6 text-gray-800">Sign Up</h2>
+        <h2 className="text-2xl font-bold text-center mb-6 text-gray-800">
+          Sign Up
+        </h2>
 
         <form className="space-y-4">
-          <div>
-            <label className="text-sm text-gray-700">Name</label>
-            <input
-              type="text"
-              placeholder="Enter your name"
-              className="w-full mt-1 p-3 rounded-xl bg-gray-50 border border-gray-300 outline-none"
-            />
-          </div>
-
-          <div>
-            <label className="text-sm text-gray-700">Email</label>
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="w-full mt-1 p-3 rounded-xl bg-gray-50 border border-gray-300 outline-none"
-            />
-          </div>
-
-          <div>
-            <label className="text-sm text-gray-700">Password</label>
-            <input
-              type="password"
-              placeholder="Enter your password"
-              className="w-full mt-1 p-3 rounded-xl bg-gray-50 border border-gray-300 outline-none"
-            />
-          </div>
+          {inputs.map((input, index) =>
+            input.options ? (
+              <div>
+                <label className="text-sm text-gray-700">{input.label}</label>
+                <select className="w-full mt-1 p-3 rounded-xl bg-gray-50 border border-gray-300 outline-none">
+                  {input.options.map((option, index) => (
+                    <option value={option}>{option}</option>
+                  ))}
+                </select>
+              </div>
+            ) : (
+              <div>
+                <label className="text-sm text-gray-700">{input.label}</label>
+                <input
+                  type="text"
+                  placeholder={input.placeholder}
+                  className="w-full mt-1 p-3 rounded-xl bg-gray-50 border border-gray-300 outline-none"
+                />
+              </div>
+            ),
+          )}
 
           <button
             type="button"
@@ -43,13 +66,12 @@ const Signup = () => {
         </form>
 
         <p className="text-center mt-6 text-sm text-gray-600">
-          Already have an account? <span className="underline cursor-pointer font-semibold">Login</span>
+          Already have an account?{" "}
+          <span className="underline cursor-pointer font-semibold">Login</span>
         </p>
       </div>
     </div>
-
-
-  )
+  );
 };
 
 export default Signup;
